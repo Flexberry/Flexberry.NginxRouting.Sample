cbb4146b7b4b94dbf58f90dc69c71335617f89c2
Valentin Churekov <VChurekov@neoplatform.ru>
2023-07-03 11:35:52 +0500

    feat: nginx.

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,7 +1,9 @@
-docker build --no-cache -f Dockerfiles/Dockerfile.PostgreSql -t nginxsample/postgre-sql SQL
+docker build -f Dockerfiles/Dockerfile.PostgreSql -t nginxsample/postgre-sql SQL
 
-docker build --no-cache -f Dockerfiles/Dockerfile.ClickhouseAudit -t nginxsample/audit-clickhouse SQL
+docker build -f Dockerfiles/Dockerfile.ClickhouseAudit -t nginxsample/audit-clickhouse SQL
 
-docker build --no-cache -f Dockerfiles/Dockerfile.App -t nginxsample/app ..
+docker build -f Dockerfiles/Dockerfile.App -t nginxsample/app ..
 
-docker build --no-cache -f Dockerfiles/Dockerfile.BackendForReadAudit -t nginxsample/auditodatabackend ..
+docker build -f Dockerfiles/Dockerfile.BackendForReadAudit -t nginxsample/auditodatabackend ..
+
+docker build -f Dockerfiles/Dockerfile.Nginx -t nginxsample/nginx Nginx
